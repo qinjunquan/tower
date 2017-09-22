fe308e5fcec0169595f724b747ef3514b1c737fa
vik <vik.qjq@gmail.com>
2017-09-22 18:02:49 +0800

    user: set current

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_current_user
+  protected
+
+  def set_current_user
+    User.current = current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   has_many :user_project_ships
   has_many :teams, :through => :user_team_ships
   has_many :projects, :through => :user_project_ships
+
+  class << self
+    def current
+      Thread.current[:user]
+    end
+
+    def current=(user)
+      Thread.current[:user] = user
+    end
+  end
 end
