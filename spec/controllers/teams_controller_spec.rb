cb54df84f9afba0177df104ef61223648c6a6798
vik <vik.qjq@gmail.com>
2017-09-26 14:30:21 +0800

    todo_list: add rspec

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'pry'
 
 RSpec.describe TeamsController, :type => :controller do
   context "test index action" do
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "test create_event" do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @team = FactoryGirl.create(:team, :creator => @user)
+      @project = FactoryGirl.create(:project, :team => @team)
+      User.current = @user
+    end
+
+    it "should be create event after create" do
+      @list = FactoryGirl.create(:todo_list, :creator => @user, :project => @project)
+      events = Event.where(:resource_id => @list.id, :resource_type => "TodoList")
+      expect(events.count).to eq(1)
+      event = events.first
+      expect(event.action).to eq(Event::ACTION["create"])
+      expect(event.user_id).to eq(@user.id)
+      expect(event.project_id).to eq(@project.id)
+      expect(event.team_id).to eq(@team.id)
+      expect(event.category_type).to eq(@project.class.to_s)
+      expect(event.category_id).to eq(@project.id)
+      expect(event.resource_changes.blank?).to eq(true)
+      expect(event.sub_resource_id.nil?).to eq(true)
+      expect(event.sub_resource_type.nil?).to eq(true)
+    end
+  end
 end
